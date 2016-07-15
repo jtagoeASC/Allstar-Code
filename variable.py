a6cffca29f94d4baf5623f8fcb3e9ad89d478801
jtagoeASC <tagoej@bxscience.edu>
2016-07-15 10:02:54 -0400

    Me and Dannon
    Had to find the blob

---
+++ variable.py
@@ -0,0 +1,13 @@
+from Myro import *
+init("sim")
+penDown()
+Speed=1
+Seconds=4
+Angle=90
+forward(Speed,Seconds)
+turnBy(Angle)
+forward(Speed,Seconds)
+turnBy(Angle)
+forward(Speed,Seconds)
+turnBy(Angle)
+forward(Speed,Seconds)
