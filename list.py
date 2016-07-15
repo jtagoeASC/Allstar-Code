151dc93f814b17076d4cf909db34685df8a33e36
jtagoeASC <tagoej@bxscience.edu>
2016-07-15 11:22:02 -0400

    List

---
+++ list.py
@@ -0,0 +1,24 @@
+myList=[10,11,12,13,14,15,16,17,18,19,20]
+
+myOtherList=[0,0,0,0,0]
+
+myEmptyList=[]
+
+myOtherList=[]
+i=0
+while i<5:
+    myOtherList
+    print(myOtherList)
+    i=i+1
+    
+theFifthList=["a",5,"doodle",3,10]   
+print(len(theFifthList))
+
+theFifthList.append(90)
+print(theFifthList)
+
+theFifthList[0]=8.4
+print(theFifthList)
+
+theFifthList.insert(0,98)
+print(theFifthList)
