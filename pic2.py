1307fc8c6b255c2a30f8f684058f3b7fdc72b0cc
jtagoeASC <tagoej@bxscience.edu>
2016-07-18 09:18:16 -0400

    Me and sam almote
    We took a picture off the internet and obamified it

--- pic2.py
+++ pic2.py
@@ -3,9 +3,11 @@ ObamaDarkBlue = makeColor(0,51,76)
 ObamaRed = makeColor(217, 26, 33)
 ObamaBlue = makeColor(112,150,158)
 ObamaYellow = makeColor(252, 227, 166)
-Ab=makePicture("xoCDeFzV_400x400.jpeg")
+Ab=makePicture("lebron.jpg")
 i=0
+#Gets pixels from picture
 PXL= getPixels(Ab)
+#Gets gray value from every pixel and changes pixel to differnt color
 for p in PXL:
     grayValue = getGray(p)
     if grayValue > 180:
